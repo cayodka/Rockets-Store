bdb7dbd624b1cd84019edcc677f52c0c8a41b14d
unknown <cayodka00@gmail.com>
2024-12-30 23:37:19 -0300

    nhem

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,18 +1,36 @@
 const express = require("express");
-const fs = require("fs"); 
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
-app.use(express.static(path.join(__dirname, '__', 'frontend')))
 
-app.get('/getPedidos', (pedido, resposta) => { })
-app.get('postPedidos', (pedido, resposta) => { })
 
-app.post('/postMensagens', (pedido, resposta) => { })
+app.use(express.static(path.join(__dirname)));
 
-app.get('/getSobreNos', (pedido, resposta) => { })
-app.post('/postSobreNos', (pedido, resposta) => { })
 
-app.listen(1010, ( ) => {
-    console.log("http://localhost:1010")
-})
+app.get('/listarArquivos', (req, res) => {
+    
+    const files = [];
+    
+    
+    fs.readdirSync(path.join(__dirname)).forEach(file => {
+        files.push(file);
+    });
+    
+    
+    fs.readdirSync(path.join(__dirname, 'frontend')).forEach(file => {
+        files.push(`frontend/${file}`);
+    });
+    
+    
+    fs.readdirSync(path.join(__dirname, 'frontend', 'areaAdminstrador')).forEach(file => {
+        files.push(`frontend/areaAdminstrador/${file}`);
+    });
+
+    res.json(files); 
+});
+
+
+app.listen(1010, () => {
+    console.log("Servidor rodando em http://localhost:1010");
+});
